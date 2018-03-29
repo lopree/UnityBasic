a2a28c158680d21c1b0bbc5a20bb1f65519b326b
lopree <zhutong1993@gmail.com>
2018-03-29 15:16:54 +0800

    对象池

---
+++ ObjectPool/Assets/Scripts/FirstScr.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScr : MonoBehaviour
+{
+
+    #region 对象池
+    List<GameObject> pools01;
+    Dictionary<int, List<GameObject>> pools02;
+    private static FirstScr Singlton;
+    #endregion
+}
